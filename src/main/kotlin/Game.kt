d623214baaf0ad24b3250d886c430cdfeb791ee4
hitak <hitak77@naver.com>
2023-11-28 04:48:44 +0900

    clear code

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,14 @@
 import java.text.NumberFormat
 import java.util.Locale
 
+class Game{
+    var money: Int = 10000 //초기 자금
+    val sheet: MutableList<MutableList<Int>> = mutableListOf()
+    
+    fun oneGame() {
+
+        while(true){
+
+        }
+    }
+}
---
+++ src/main/kotlin/Lotto.kt
@@ -0,0 +1,35 @@
+import java.util.*
+
+class Lotto {
+    public fun AutoLotto(): MutableList<Int> {
+        val lotto = mutableListOf<Int>()
+
+        while(lotto.size < 6){
+            val random = Random().nextInt(45) + 1
+            if(!lotto.contains(random))
+                lotto.add(random)
+        }
+        lotto.sort()
+        return lotto
+    }
+
+    public fun ManualLotto(): MutableList<Int> {
+        var lotto = mutableListOf<Int>()
+
+        while(lotto.size < 6){
+            print("번호를 입력해 주세요. : ")
+            val input: String = readLine().toString()
+            val arr = input.split(" ").filter{ it.isNotBlank() && it.matches(Regex("-?\\d+")) }
+
+            arr.map{ it.toInt() }.forEach{
+                if(it in 1..45 && it !in lotto && lotto.size < 6)
+                    lotto.add(it)
+                if(lotto.size >= 6)
+                    return@forEach
+            }
+        }
+
+        lotto.sort()
+        return lotto
+    }
+}
