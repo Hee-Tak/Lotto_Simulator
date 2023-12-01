6d5de4714f14b1dcd6ee63b418d27d839a0eefe8
hitak <hitak77@naver.com>
2023-12-01 21:20:28 +0900

    clear code 수정중

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -9,8 +9,58 @@ class Game{
 
         while(true){
             println("================================================================================================================")
+            println(" [1:보유중인Sheets확인\t2:로또구매\t3:당첨번호확인\t4:종료\t5:무료충전\t6:DB다루기]")
             println("================================================================================================================")
             print("=> ")
+            val choose = readLine().toString().trim().toInt()
+            when(choose){
+                1 -> {
+                    checkSheets()
+                }
+
+                2 -> {
+                    purchaseLotto()
+                }
+
+                3 -> {
+                    checkResult()
+                }
+                4 -> {
+                    exit()
+                }
+                5 -> {
+                    freeCharge()
+                }
+                6 -> {
+                    DB()
+                }
+            }
         }
     }
+
+
+    fun checkSheets(){
+
+    }
+
+    fun purchaseLotto(){
+
+    }
+
+    fun checkResult(){
+
+    }
+
+    fun exit(){
+
+    }
+
+    fun freeCharge(){
+
+    }
+
+    fun DB(){
+
+    }
+
 }
